485599c3478a8440f9b54d21d732afa71a46ac80
Rakshit Arora <rakshit087@gmail.com>
2021-11-11 23:36:32 +0530

    Tailwindified FileIcon.tsx

--- kode-editor-client/src/components/FileIcon.tsx
+++ kode-editor-client/src/components/FileIcon.tsx
@@ -1,17 +1,16 @@
 import {DiCode, DiPython, DiJavascript1, DiHtml5, DiCss3Full, DiSass, DiLess} from 'react-icons/di'
-import './FileIcon.css'
 
 export default function FileIcon(props: {type: string}): JSX.Element {
     return(
         <>
-        {props.type==='python'&&<DiPython className='fileIcon' />}
-        {props.type==='javascript'&&<DiJavascript1 className='fileIcon' />}
-        {props.type==='html'&&<DiHtml5 className='fileIcon' />}
-        {props.type==='css'&&<DiCss3Full className='fileIcon' />}
-        {props.type==='cpp'&&<DiCode className='fileIcon' />}
-        {props.type==='sass'&&<DiSass className='fileIcon' />}
-        {props.type==='less'&&<DiLess className='fileIcon' />}
-        {props.type==='other'&&<DiCode className='fileIcon' />}
+        {props.type==='python'&&<DiPython className='mr-1 text-lg' />}
+        {props.type==='javascript'&&<DiJavascript1 className='mr-1 text-lg' />}
+        {props.type==='html'&&<DiHtml5 className='mr-1 text-lg' />}
+        {props.type==='css'&&<DiCss3Full className='mr-1 text-lg' />}
+        {props.type==='cpp'&&<DiCode className='mr-1 text-lg' />}
+        {props.type==='sass'&&<DiSass className='mr-1 text-lg' />}
+        {props.type==='less'&&<DiLess className='mr-1 text-lg' />}
+        {props.type==='other'&&<DiCode className='mr-1 text-lg' />}
         </>
     );
 }
